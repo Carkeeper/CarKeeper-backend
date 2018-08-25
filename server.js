6c564ce70ea03ada5bca18d30897279412072c28
thebigr <nevoroy@gmail.com>
2018-08-25 10:28:00 +0300

    started adding https and passport. still missing - routes, certificates.

--- server.js
+++ server.js
@@ -2,5 +2,29 @@ const path = require('path');
 const express = require('express');
 const errorHandler = require('./errorHandling');
 const app = express();
+require('./common.middlewares').addCommonMiddlewares(app);
+require('./config/passport.config').init();
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const protocols = require('./config/protocols.config');
 
-app.use(express.json());
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.clearCookie('connect.sid');
+    res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+
+http.createServer(app).listen(protocols.httpPort,
+    () => console.log(`http on port ${protocols.httpPort}`));
